776b5d807a2bad6435e96605345ed5794f7c4e07
llango <724261350@qq.com>
2022-11-25 16:40:16 +0800

    Update __init__.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,13 +1,7 @@
 """{{ cookiecutter.project_name }} namespace."""
 
-from importlib_metadata import PackageNotFoundError, version
 
 __author__ = "{{ cookiecutter.full_name }}"
 __email__ = "{{ cookiecutter.email }}"
+__version__ = "{{ cookiecutter.project_version }}"
 
-# 用于从github动作中自动设置版本号 
-# 以及在本地测试时不中断
-try:
-    __version__ = version(__package__)  # type: ignore
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "0.0.0"
